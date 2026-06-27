375cb3db095113a832eeb6e2c1591a59c19db609
inno-inho <zergling2565@gmail.com>
2026-06-28 02:43:27 +0900

    Check at my home

--- backEnd/src/main/java/com/example/demo/config/WebConfig.java
+++ backEnd/src/main/java/com/example/demo/config/WebConfig.java
@@ -12,7 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
         corsRegistry.addMapping("/**")      // 全ての経路に関して
-                .allowedOrigins("http://localhost:5173")   // FrontEndのURL許可
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://127.0.0.1:5173"
+//                        "http://[::1]:5173"       // FrontEnd's URL
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
